40651de07317cd811e5718559fefb029556eecdb
Vitor Vieira Freire <vitorvfreire@outlook.com>
2023-06-11 11:31:10 -0300

    upload,remove,file

--- API/helpers.py
+++ API/helpers.py
@@ -1,22 +1,40 @@
 import time
+import os
+import pathlib
+from flask import send_from_directory
 
 class file_class:
-    def __init__(self,file):
-        self.file=file
+    def __init__(self, file):
+        self.file = file
     
     def save_file(self):
         try:
-            type_file=type(self.file)
-            time_now=time.timezone()
-            if type_file=='png':
-                name=f'img{self.file}{time_now}'
-                path=f'file/img/png/{name}'
-            elif type_file=='mp4':
-                name=f'video{self.file}{time_now}'
-                path=f'file/video/mp4/{name}'
-            else:
-                return False
-            self.file.save(path)
-        except:
+            filename = self.file.filename
+            time_now = int(time.time())
+            name = f'files/{time_now}{filename}'           
+            self.file.save(name)
+            return True
+        except Exception as e:
+            print("**ERRO::", e)
             return False
+        
+    def remove_file(self):
+        try:
+            diretorio = pathlib.Path('files')
+            name = f'{self.file.filename}'
+            arquivo = list(diretorio.glob(f'*{name}*'))
+            os.remove(arquivo[0])
+            return True
+        except Exception as e:
+            print("**ERRO::", e)
+            return False
+    
+    def get_file_path(self):
+        diretorio = pathlib.Path('files')
+        name = f'{self.file}'
+        arquivo = list(diretorio.glob(f'*{name}*'))
+        if arquivo:
+            return str(arquivo)
+        else:
+            return 'Arquivo não encontrado'
     
--- API/routes.py
+++ API/routes.py
@@ -1,15 +1,36 @@
-from flask import Flask, request
+from flask import Flask, request, url_for, send_from_directory,jsonify
 from app import app
 from helpers import file_class
 
-@app.post('/upload')
+@app.route('/upload', methods=['POST'])
 def upload():
     if 'file' not in request.files:
-        return 'Nenhum vídeo enviado', 400
+        return "file wans't posted", 400
     
-    file=file_class(request.files['video'])
+    file = file_class(request.files['file'])
 
-    if file.save_file:
-        return "file wasn't saved"
+    if not file.save_file():
+        return "file wans't saved", 400
 
-    return 'file saved'
+    return 'file saved', 200
+
+@app.route('/remove', methods=['POST','DELETE'])
+def remove():
+    if 'file' not in request.files:
+        return "file wans't posted", 400
+    
+    file = file_class(request.files['file'])
+
+    if not file.remove_file():
+        return "file wans't removed", 400
+
+    return 'file removed', 200
+
+@app.route('/files', methods=['POST'])
+def get_files():
+    if 'file' not in request.json:
+        return 'Nenhum arquivo enviado', 400
+    
+    file = file_class(request.json['file'])
+    return jsonify(file.get_file_path())
+    #return send_from_directory(file.get_file_path()) 
